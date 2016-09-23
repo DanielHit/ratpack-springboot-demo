e56ad0a98d612b5b2f589e7331ea14a289029ecd
hulei02 <hulei02@meituan.com>
2016-09-23 14:11:44 +0800

    add json request and json response

--- src/main/java/com/qiezi/demo/handler/FooHandler.java
+++ src/main/java/com/qiezi/demo/handler/FooHandler.java
@@ -1,10 +1,12 @@
 package com.qiezi.demo.handler;
 
+import com.qiezi.demo.model.People;
 import com.qiezi.demo.vo.RequestVo;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 
 import static ratpack.jackson.Jackson.fromJson;
+import static ratpack.jackson.Jackson.json;
 
 /**
  * Created by Daniel on 9/23/16.
@@ -12,6 +14,6 @@ import static ratpack.jackson.Jackson.fromJson;
 public class FooHandler implements Handler {
     @Override
     public void handle(Context ctx) throws Exception {
-        ctx.render(ctx.parse(fromJson(RequestVo.class)).map(p -> p.getName()));
+        ctx.render(ctx.parse(fromJson(RequestVo.class)).map(p -> json(new People(p.getName(), 23))));
     }
 }
