86b3c835052394aa6ede6c90fc451615b7ebe30f
hulei02 <hulei02@meituan.com>
2016-09-23 12:02:19 +0800

    set port special

--- src/main/java/com/qiezi/demo/Main.java
+++ src/main/java/com/qiezi/demo/Main.java
@@ -2,18 +2,26 @@ package com.qiezi.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ratpack.server.RatpackServer;
+import ratpack.server.ServerConfig;
 
 /**
  * Created by Daniel on 9/22/16.
  */
 @SpringBootApplication
 public class Main {
-    public static void main(String... args) throws Exception {
-        RatpackServer.start(server -> server
-                .handlers(chain -> chain
-                        .get(ctx -> ctx.render("Hello World!"))
-                        .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
-                )
-        );
-    }
+
+        public static void main(String... args) throws Exception {
+
+            RatpackServer.start(server -> server
+                    .serverConfig(ServerConfig.embedded().port(8080))
+                    .registryOf(registry -> registry.add("World!"))
+                    .handlers(chain -> chain
+                            .get(ctx -> ctx.render("Hello " + ctx.get(String.class)))
+                            .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
+                            .get("a", ctx -> ctx.render("fuck"))
+                    )
+            );
+
+        }
+
 }
